210c262b04e77b59baa28fa5fb43843b3084e6f9
Lawalolabode <lawalsulaimon003@gmail.com>
2025-04-12 13:56:37 +0100

    new menstrual

--- PhoneBook/phoneBook_method.py
+++ PhoneBook/phoneBook_method.py
@@ -1,3 +1,7 @@
+import string
+from operator import contains
+
+
 class phoneBook:
     def __init__(self):
         self.phoneBook = {}
@@ -5,29 +9,58 @@ class phoneBook:
         self.phoneNumber = ""
         self.firstName = ""
         self.lastName = ""
+        self.email = ""
+        self.counter = 0
+
+    def check_empty(self):
+        if self.counter == 0:
+            return True
+        return False
 
 
     def add_contact(self, phone_number: str, first_name: str, last_name: str):
         if not phone_number.isnumeric():
            raise ValueError("Invalid phone number")
         self.phoneNumber = phone_number
-        if not isinstance(first_name, str):
-            raise ValueError("First name must be a string")
         self.firstName = first_name
-        if not isinstance(last_name, str):
-            raise ValueError("Last name must be a string")
         self.lastName = last_name
+        result = (f'''
+        firstname: {self.firstName} 
+        lastname : {self.lastName}
+        phone number: {phone_number}
+        email: {self.email}
+        ''')
+        self.phoneBook[phone_number] = result
+        self.counter += 1
 
-        self.phoneBook[self.phoneNumber] = self.firstName + " " + self.lastName
+    def get_numbers_of_contact(self):
+        return self.counter
 
-    def get_phone_number(self):
-        return self.phoneNumber
+    def remove_contact(self, phone_number: str):
+        if phone_number in self.phoneBook:
+            self.phoneBook.pop(phone_number)
 
-    def get_first_name(self):
-        return self.firstName
 
-    def get_last_name(self):
-        return self.lastName
+    def set_email(self, email: str):
+        if not isinstance(email, str):
+            raise ValueError("Email must be a string")
+        if  '@' in email and email.count('@') == 1 and '.'  in email.split('@')[1]:
+            self.email = email
+        else:
+            raise ValueError("Email must be a valid email address")
+
+
+
+    def get_contact_by_phone_number(self, input_phone_number: str ):
+        if input_phone_number in self.phoneBook:
+            return self.phoneBook[input_phone_number]
+        else:
+            raise ValueError("Invalid phone number")
+
+
+
+
+
 
 
 
--- PhoneBook/test_phoneBook.py
+++ PhoneBook/test_phoneBook.py
@@ -6,9 +6,44 @@ class TestPhoneBook(unittest.TestCase):
       def test_phoneBook(self):
           contact = phoneBook()
 
-      def test_phoneBook_add_contact(self):
+      def test_is_empty(self):
           contact = phoneBook()
-          contact.add_contact("", "<EMAIL>","")
+          self.assertTrue(contact.check_empty())
+
+      def test_add_contact(self):
+          contact = phoneBook()
+          contact.add_contact("09023456788", "ole","you")
+          self.assertFalse(contact.check_empty())
+
+      def test_to_get_phoneBook_size(self):
+          contact = phoneBook()
+          self.assertEqual(0, contact.get_numbers_of_contact())
+          contact.add_contact("09023456788", "ole","you")
+          self.assertEqual(1, contact.get_numbers_of_contact())
+
+      def test_to_remove_contact(self):
+          contact = phoneBook()
+          contact.add_contact("09023456788", "ole", "you")
+          contact.remove_contact("09023456788")
+          self.assertEqual(1, contact.get_numbers_of_contact())
+
+      def test_to_get_contact_by_phoneNumber(self):
+          contact = phoneBook()
+          contact.add_contact("09023456788", "ole", "you")
+          contact.set_email("olabodelawal@gmail.com")
+          result =  '''
+          firstname: ole 
+          lastname : you
+          phone number: 09023456788
+          email: olabodelawal@gmail.com
+          '''
+          self.assertEqual(result, contact.get_contact_by_phone_number("09023456788"))
+
+
+
+
+
+
 
 
 
